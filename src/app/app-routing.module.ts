b88dafed33c2d1fcd7bcbdc8a6c99d2af994b396
joserobertoarias2005 <joserobertoarias2005@hotmail.com>
2022-07-03 20:33:48 -0600

    deploying docs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { MainPageComponent } from './components/main-page/main-page.component';
 
 const routes: Routes = [
   { path: '', component:MainPageComponent},
-  { path: '**', redirectTo: '/'}
+  { path: '**', redirectTo: ''}
 ];
 
 @NgModule({
